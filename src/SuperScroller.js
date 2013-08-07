b0fab4babb36ab2cc7b88cfe3683016e16eb0c54
Fahim Chowdhury <fahim.chowdhury@ogilvy.com>
2013-08-07 15:51:49 +0100

    done the sticky when scrolling down

--- src/SuperScroller.js
+++ src/SuperScroller.js
@@ -4,16 +4,49 @@ var SuperScroller = function() {
 	var _ = SuperScroller.prototype;
 
 	_.handlers = [];
+	_.previousScrollPosition = 0;
 	_.scrollPosition = 0;
 	_.scrollbar = null;
 	_.scrollhandle = null;
 	_.holder = null;
+	_.container = null;
 	_.speed = 1;
 	_.duration = 0;
+	_.holderHeight = 0;
+	_.currentIndex = 0;
+	_.atts = {
+		sticky : "data-sticky-frames",
+		top : "data-top"
+	}
+	_.isSticky = false;
+	_.releaseStickyPosition = 0;
+	_.children = [];
+	_.holderTop = 0;
+	_.totalHeight = 0;
+	_.scrollDirection = 0;
+	// _.startTime = 0;
+	// _.frameRate = 0.016;
 	_.init = function(holder) {
 		this.holder = holder;
+		this.container = document.createElement("div");
+		this.setContainer();
+		this.setHolder();
 		this.setDuration(this.speed);
 		this.createScroller();
+		this.getChildren();
+	}
+	_.setHolder = function() {
+		this.holder.parentNode.removeChild(this.holder);
+		this.holder.style.position = "absolute";
+		this.container.appendChild(this.holder);
+	}
+	_.setContainer = function() {
+		this.container.style.position = "relative";
+		this.container.style.width = "100%";
+		this.container.style.overflow = "hidden";
+		this.container.style.height = "100%";
+
+		this.holder.parentNode.appendChild(this.container);
 	}
 	_.createScroller = function() {
 		document.body.style.overflow = "hidden";
@@ -65,35 +98,44 @@ var SuperScroller = function() {
 		var root = this;
 		Utensil.addListener(document, "mouseup", this.getHandler("scrollerMouseUp"));
 		Utensil.addListener(document, "mousemove", this.getHandler("scrollerMouseMove"));
-	}, _.scrollerMouseUp = function() {
+		this.startTime = new Date().getTime();
+	}
+	_.scrollerMouseUp = function() {
 
 		Utensil.removeListener(document, "mouseup", this.getHandler("scrollerMouseUp"));
 		Utensil.removeListener(document, "mousemove", this.getHandler("scrollerMouseMove"));
-	}, _.mouseLeave = function(event) {
+	}
+	_.mouseLeave = function(event) {
 		if (event.toElement == null && event.relatedTarget == null)
 			this.getHandler("scrollerMouseUp")(event);
-	}, _.mouseWheel = function(event) {
+	}
+	_.mouseWheel = function(event) {
 		var delta = event.detail ? event.detail * (-120) : event.wheelDelta//check for detail first so Opera uses that instead of wheelDelta
-		var previousScrollPosition = this.scrollPosition;
+		this.previousScrollPosition = this.scrollPosition;
 		this.scrollPosition -= delta > 0 ? 10 : -10;
 		this.checkScrollPosition();
 		this.moveHandle();
 		this.moveHolder();
-		this.setCurrentIndex();
 
-	}, _.scrollerMouseMove = function(event) {
-		var previousScrollPosition = this.scrollPosition;
+		this.setCurrentIndex();
+		this.getHolderHeight();
+		this.checkSticky();
+	}
+	_.scrollerMouseMove = function(event) {
+		this.previousScrollPosition = this.scrollPosition;
 		this.scrollPosition = Utensil.mouseY(document.body, event);
 		this.checkScrollPosition();
 		this.moveHandle();
 
 		this.moveHolder();
 		this.setCurrentIndex();
+		this.getHolderHeight();
+		this.checkSticky();
+
 	}
 	_.setDuration = function(speed) {
 		//formula time = distance / speed
 		this.duration = this.holder.clientHeight / speed;
-		console.log(this.duration);
 
 	}
 	_.checkScrollPosition = function() {
@@ -108,31 +150,96 @@ var SuperScroller = function() {
 	}
 	_.moveHolder = function() {
 		//calculate percentage of scroller
-		var scrollPercentage = this.currentScrollPosition() / Utensil.stageHeight();
-		this.holder.style.top = -(scrollPercentage * this.holder.clientHeight) + "px";
+		var diff = (Math.abs(this.previousScrollPosition - this.scrollPosition) / Utensil.stageHeight()) * this.holder.clientHeight;
+		var hTop = this.getScrollPercentage() * this.totalHeight;
+		console.log(hTop);
+		if (this.previousScrollPosition > this.scrollPosition)
+			diff = -diff;
+		//var scrollPercentage = this.currentScrollPosition() / Utensil.stageHeight();
+		//this.holder.style.top = -(scrollPercentage * (this.getHolderHeight() - Utensil.stageHeight())) + "px";
+		this.holderTop = hTop;
+		if (this.sticky)
+			return;
+		this.holder.style.top = -(this.getHolderTop() + diff) + "px";
+	}
+	_.checkSticky = function() {
+		var currentItem = this.children[this.currentIndex];
+		console.log(currentItem.getAttribute(this.atts.sticky));
+		if (currentItem.getAttribute(this.atts.sticky)) {
+			var framerate = Number(currentItem.getAttribute(this.atts.sticky));
+			this.releaseStickyPosition = Number(currentItem.getAttribute(this.atts.top)) + (framerate * currentItem.clientHeight);
+			this.sticky = true;
+			console.log("STICK IS TRUE");
+		} else {
+			this.sticky = false;
+			console.log("STICK IS FALSE")
+		}
+
+		var holderCurrentTop = this.getHolderTop();
+		
+		if (this.holderTop >= this.releaseStickyPosition) {
+			this.sticky = false;
+		}
+		// if(this.releaseStickyPosition)
 	}
 	_.currentScrollPosition = function() {
 		var top = Number(this.scrollhandle.style.top.replace("px", ""));
 
 		return top + this.scrollhandle.clientHeight;
 	}
+	_.getScrollPercentage = function() {
+		var top = Number(this.scrollhandle.style.top.replace("px", "")) + this.scrollhandle.clientHeight;
+		var scrollPercentage = top / Utensil.stageHeight();
+		return scrollPercentage;
+	}
 	_.setCurrentIndex = function() {
 
+		for (var a = 0; a < this.children.length; a++) {
+			var element = this.children[a];
+
+			var currentTop = this.getOffset(element).top;
+			var currentParentTop = this.getOffset(this.holder).top;
+			if (currentTop <= 0 && this.currentIndex != a) {
+				this.currentIndex = a;
+				console.log("CHANGED_INDEX", a);
+			}
+
+		}
+	}
+	_.getChildren = function() {
+		var previousTop = 0;
 		for (var a = 0; a < this.holder.childNodes.length; a++) {
 			var element = this.holder.childNodes[a];
-			
 			if (element.tagName == "DIV") {
-				var currentTop = this.getOffset(element).top;
-				var currentParentTop = this.getOffset(this.holder).top;
-				if (currentTop <= 0) {
-					//element.className+=" "+this.classNames.STICKY;
-					//this.stickyItems[a].defaultTop=currentParentTop;
-					//this.setSticky(element);
-					//this.executeCallbacks(this.events.ON_STICKY);
-					console.log("new index",a);
-				} 
+				var stickFrames = Number(element.getAttribute(this.atts.sticky));
+				element.setAttribute(this.atts.top, previousTop);
+				element.style.height=Utensil.stageHeight()+"px";
+				this.children.push(element);
+
+				previousTop += element.clientHeight + ( stickFrames ? (stickFrames * element.clientHeight) : 0);
 			}
 		}
+	}
+	_.getHolderTop = function() {
+		var top = Math.abs(this.holder.style.top.replace("px", ""));
+		if (!top)
+			top = 0;
+		return top;
+	}
+	_.getHolderHeight = function() {
+		var h = this.holder.clientHeight;
+		//loop children and find sticky
+		for (var a = 0; a < this.holder.childNodes.length; a++) {
+			var element = this.holder.childNodes[a];
+
+			if (element.getAttribute && element.getAttribute(this.atts.sticky)) {
+				var stickFrames = Number(element.getAttribute(this.atts.sticky));
+				h = h + (stickFrames * element.clientHeight);
+			}
+		}
+		this.totalHeight = h;
+		return h;
+
 	}
 	_.getOffset = function(el) {
 		var _x = 0;
